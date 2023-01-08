40c7945b9f03c3872df8e3b039ada2e694cea256
Seafoxlrt616 <103425286+Seafoxlrt616@users.noreply.github.com>
2023-01-08 23:41:47 +0800

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -1120,27 +1120,27 @@ ENV_DEFS[SIM_MODE_EXPERIMENTAL].SST = {
     }
 };
 ENV_DEFS[SIM_MODE_WPAC].SST = {
-	mapFunc: (u,x,y,z)=>{
-        if(y<0) return 0;
-        let anom = u.field('SSTAnomaly');
-        let s = seasonalSine(z);
-        let w = map(cos(map(x,0,WIDTH,0,PI)),-0.8125,0.8125,0.8125,0.8125);
-        let h0 = y/HEIGHT;
-        let h1 = (sqrt(h0)+h0)/2;
-        let h2 = sqrt(sqrt(h0));
-        let h = map(cos(lerp(PI,0,lerp(h1,h2,sq(w)))),-1,1,0,1);
-        let ospt = u.modifiers.offSeasonPolarTemp;
-        let pspt = u.modifiers.peakSeasonPolarTemp;
-        let ostt = u.modifiers.offSeasonTropicsTemp;
-        let pstt = u.modifiers.peakSeasonTropicsTemp;
-        let t = lerp(map(s,-1,1,ospt,pspt),map(s,-1,1,ostt,pstt),h);
-        return t+anom;
-    },
+//	mapFunc: (u,x,y,z)=>{
+//        if(y<0) return 0;
+//        let anom = u.field('SSTAnomaly');
+//        let s = seasonalSine(z);
+//        let w = map(cos(map(x,0,WIDTH,0,PI)),-0.8125,0.8125,0.8125,0.8125);
+//        let h0 = y/HEIGHT;
+//        let h1 = (sqrt(h0)+h0)/2;
+//        let h2 = sqrt(sqrt(h0));
+//        let h = map(cos(lerp(PI,0,lerp(h1,h2,sq(w)))),-1,1,0,1);
+//        let ospt = u.modifiers.offSeasonPolarTemp;
+//        let pspt = u.modifiers.peakSeasonPolarTemp;
+//        let ostt = u.modifiers.offSeasonTropicsTemp;
+//        let pstt = u.modifiers.peakSeasonTropicsTemp;
+//        let t = lerp(map(s,-1,1,ospt,pspt),map(s,-1,1,ostt,pstt),h);
+//        return t+anom;
+//    },
     modifiers: {
         peakSeasonPolarTemp: 0,
         offSeasonPolarTemp: -3,
         offSeasonTropicsTemp: 26.8,
-        peakSeasonTropicsTemp: 29.75
+        peakSeasonTropicsTemp: 29
     }
 };       
 ENV_DEFS[SIM_MODE_EXTREME].SST = {
